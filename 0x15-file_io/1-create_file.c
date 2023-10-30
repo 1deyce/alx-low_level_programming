d94bad52e6230198deb3fc8a1ef35026aa3690f0
1deyce <124916049+1deyce@users.noreply.github.com>
2023-10-30 17:06:20 +0200

    Update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,13 +4,12 @@
 
 int create_file(const char *filename, char *text_content) {
     int fd;
-    ssize_t n_written;
-    size_t len = 0;
+    ssize_t n_written, len = 0;
 
     if (filename == NULL)
         return (-1);
 
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+    fd = open(filename, O_WRONLY | O_CREAT, 0600);
     if (fd == -1)
         return (-1);
 
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -4,8 +4,7 @@
 
 int append_text_to_file(const char *filename, char *text_content) {
     int fd;
-    ssize_t n_written;
-    size_t len = 0;
+    ssize_t n_written, len = 0;
 
     if (filename == NULL)
         return (-1);
